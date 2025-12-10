d1f7d8264c3d40be1f477e9e00cf6b4d9aa0b1ba
David <uo289792@uniovi.es>
2025-12-10 14:01:39 +0100

    cambios nav

--- php/test.php
+++ php/test.php
@@ -136,18 +136,16 @@ if ($_SERVER['REQUEST_METHOD']==='POST' && isset($_POST['accion'])){
 <body>
     <main>       
     <header>
-            <h1>MotoGP Desktop</h1>
-            
-            <nav aria-label="Menú principal">
-                <a href="index.html" title="Inicio" class="active">Inicio</a>
-                <a href="piloto.html" title="Información del piloto">Piloto</a>
-                <a href="circuito.html" title="Información de circuitos">Circuito</a>
-                <a href="meteorologia.html" title="Información meteorológica">Meteorología</a>
-                <a href="clasificaciones.php" title="Clasificaciones">Clasificaciones</a>
-                <a href="juegos.html" title="Juegos">Juegos</a>
-                <a href="ayuda.html" title="Ayuda del proyecto">Ayuda</a>
-            </nav>
-            
+        <h1>MotoGP Desktop</h1> 
+        <nav aria-label="Menú principal">
+            <a href="index.html" title="Inicio" class="active">Inicio</a>
+            <a href="piloto.html" title="Información del piloto">Piloto</a>
+            <a href="circuito.html" title="Información de circuitos">Circuito</a>
+            <a href="meteorologia.html" title="Información meteorológica">Meteorología</a>
+            <a href="clasificaciones.php" title="Clasificaciones">Clasificaciones</a>
+            <a href="juegos.html" title="Juegos">Juegos</a>
+            <a href="ayuda.html" title="Ayuda del proyecto">Ayuda</a>
+        </nav>
     </header>
 
     <p class="migas">Estás en: <a href="index.html">Inicio</a> >> <a href="juegos.html">Juegos</a> >> Test de Usabilidad</p>
